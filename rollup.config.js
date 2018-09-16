314c5668bddcb40a13c3d197409f2e0192d3a513
Rory Duncan <robust.rory@gmail.com>
2018-09-16 02:55:45 +0000

    Get build of suki goin'

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,17 @@
 // rollup.config.js
+import resolve from "rollup-plugin-node-resolve";
+
 export default {
   input: 'src/main.js',
   output: {
     name: "game",
     file: 'public/bundle.js',
     format: 'iife'
-  }
+  },
+  plugins: [
+    resolve({
+      main: true,
+      modulesOnly: true,
+    })
+  ],
 };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 
+import { suki } from "@roryduncan/suki";
+
+suki.whenReady(() => suki.start())
 
 console.log("hello world")
